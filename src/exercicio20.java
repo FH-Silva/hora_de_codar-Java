4f65acc956cc5bdbe77d695a183cb59493a41795
Fabio Henrique <fabiosilva5210@gmail.com>
2024-05-26 02:43:02 -0300

    atz1

--- src/exercicio20.java
+++ src/exercicio20.java
@@ -21,7 +21,33 @@ public class exercicio20 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Nome do produto: ");
+        String nome = input.next();
 
+        System.out.print("Qtd do produto: ");
+        int qtd = input.nextInt();
+
+        System.out.print("Preço: ");
+        int preco = input.nextInt();
+
+        int valorVenda = Math.round(preco * qtd + preco * 0.2f);
+        float desconto = 0f;
+        if (qtd <= 5){
+            desconto = valorVenda - preco * 0.02f;
+            System.out.println("Desconto de 2%");
+        }else if (5 < qtd && qtd <= 10){
+            desconto = valorVenda - preco * 0.03f;
+            System.out.println("Desconto de 3%");
+        }else if (qtd > 10){
+            desconto = valorVenda - preco * 0.05f;
+            System.out.println("Desconto de 5%");
+        }else{
+            System.out.println("Erro");
+        }
+        System.out.println("Produto: " + nome);
+        System.out.println("Quantidade: " + qtd);
+        System.out.println("Imposto: 20%");
+        System.out.println("Total a pagar: R$" + desconto);
         input.close();
     }
 }
